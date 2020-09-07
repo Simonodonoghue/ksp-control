47157d6cdf39b29c6912175c3456231010e02fd8
sdon <simon.p.odonoghue@accenture.com>
2020-09-07 16:32:06 +0100

    continued flight control testing

--- flight-control/flight-control.py
+++ flight-control/flight-control.py
@@ -8,66 +8,74 @@ conn = krpc.connect(name="telemtry", address="192.168.1.174")
 sc = conn.space_center
 vessel = sc.active_vessel
 
-flightParams = vessel.flight(vessel.orbit.body.reference_frame)
-pitchRate = 90 / orbitAltitude
+flightParams = vessel.flight()
     
-
-# select altitude
-
 # determine pitch rate based on desired altitude. Want to be prograde at altitude
 vessel.control.throttle = 1
 vessel.control.sas = True
 
 vessel.control.activate_next_stage()
 
+desiredPitch = 80
+
 def pitch_control():
+    global desiredPitch
+    while True:
+        print(flightParams.pitch)
+        if (flightParams.pitch < desiredPitch):
+            vessel.control.pitch = -0.4
+        elif (flightParams.pitch > desiredPitch):
+            vessel.control.pitch = 0.4
+        else:
+            vessel.control.pitch = 0
+
+def stage1_manager():
+    global desiredPitch
+
     # 10,000 - 30
     while flightParams.mean_altitude < 2000:
         pass
-
-    while flightParams.pitch < 40:
-        print(flightParams.pitch)
-        vessel.control.pitch = 1
-    vessel.control.pitch = 0
+    
+    desiredPitch = 70
 
     # 40,000 - 60
-    while flightParams.mean_altitude < 10000:
+    while flightParams.mean_altitude < 5000:
         pass
 
-    while flightParams.pitch < 60:
-        print(flightParams.pitch)
-        vessel.control.pitch = 1
-    vessel.control.pitch = 0
-    
+    desiredPitch = 60 
 
 pitch_thread = threading.Thread(target=pitch_control)
 pitch_thread.start()
+
+pitch_mthread = threading.Thread(target=stage1_manager)
+pitch_mthread.start()
 
 # watch until apoapsis is at desired altitude
 while (vessel.orbit.apoapsis_altitude < orbitAltitude):
     # determine pitch
     pass
-    
+
+# we'll now reach our altitude so we need to gain horizontal speed
+vessel.control.throttle = 0
+time.sleep(2)
 vessel.control.activate_next_stage()
 time.sleep(2)
-vessel.control.throttle = 0
 vessel.control.activate_next_stage()
 
-while vessel.flight().pitch > 0:
-    print(vessel.flight().pitch)
-    vessel.control.pitch = 1
-vessel.control.pitch = 0
+time.sleep(2)
 
+desiredPitch = -10
 
-while flightParams.mean_altitude < orbitAltitude:
-    # determine pitch
+while flightParams.pitch > 0:
     pass
 
 vessel.control.throttle = 1
 vessel.control.activate_next_stage()
 
+
+
 # once at desired apoapsis, execute circularisation burn until periapsis is at desired altitude
-while (vessel.orbit.periapsis_altitude < orbitAltitude):
+while (vessel.orbit.speed < 2295):
     pass
 
 vessel.control.throttle = 0
